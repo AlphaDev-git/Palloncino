3bca60d397e737f9004a9950c4cf807da843a4fe
Momen Massoud <alphadev32@gmail.com>
2025-09-08 13:23:00 +0300

    first commit

---
+++ lib/Core/Utils/app.images.dart
@@ -0,0 +1,36 @@
+//all App Images
+
+abstract class AppImages {
+  static const String appPLogo = "lib/core/Utils/assets/Images/Logo Op1 test Ed1.png";
+  static const String appuser = "lib/core/Utils/assets/Images/user.jpg";
+  static const String car = "lib/core/Utils/assets/Images/cars.jpg";
+  static const String contracting="lib/core/Utils/assets/Images/contracting.jpg";
+  static const String electronics="lib/core/Utils/assets/Images/electronics.jpg";
+  static const String fashion="lib/core/Utils/assets/Images/fashion.jpg";
+  static const String funiture="lib/core/Utils/assets/Images/funiture.jpg";
+  static const String jop="lib/core/Utils/assets/Images/jop.jpg";
+  static const String json="lib/core/Utils/assets/Images/waseelati-dfbad-411d23816fb4.json";
+  static const String realstate="lib/core/Utils/assets/Images/realstate.jpg";
+  static const String services="lib/core/Utils/assets/Images/he-equipment-services-inc-logo-png_seeklogo-506053.jpg";
+  static const String view_icon="lib/core/Utils/assets/Images/view_icon.png";
+  static const String ads_icon="lib/core/Utils/assets/Images/ads_icon.png";
+  static const String add_icon="lib/core/Utils/assets/Images/add.png";
+  static const String account_icon="lib/core/Utils/assets/Images/account_icon.png";
+  static const String coom_icon="lib/core/Utils/assets/Images/comm_icon.png";
+  static const String about="lib/core/Utils/assets/Images/about.png";
+  static const String sugg="lib/core/Utils/assets/Images/sugg.png";
+  static const String feed="lib/core/Utils/assets/Images/feed.png";
+  static const String whatsapp="lib/core/Utils/assets/Images/Whatsapp.png";
+  static const String upload_icon="lib/core/Utils/assets/Images/upload_icon.png";
+  static const String lang_icon="lib/core/Utils/assets/Images/lang_icon.png";
+  static const String feedback="lib/core/Utils/assets/Images/feedback.png";
+  static const String sales="lib/core/Utils/assets/Images/sales.png";
+  static const String call_us="lib/core/Utils/assets/Images/call_us.png";
+  static const String add_sug="lib/core/Utils/assets/Images/add_sug.png";
+  static const String help="lib/core/Utils/assets/Images/help.png";
+  static const String facebook="lib/core/Utils/assets/Images/facebook.png";
+  static const String insta="lib/core/Utils/assets/Images/insta.png";
+  static const String x="lib/core/Utils/assets/Images/x.png";
+  static const String linkedin="lib/core/Utils/assets/Images/linkedin.png";
+  static const String tiktok="lib/core/Utils/assets/Images/tiktok.png";
+}
